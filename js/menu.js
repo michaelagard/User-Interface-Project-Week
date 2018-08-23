53d393bc06d1246f546795d1fe7dd5332e9ef46c
Michael Agard <mdagard@gmail.com>
2018-08-23 16:20:33 -0400

    added functionality to hide header background once menu is clicked.

--- js/menu.js
+++ js/menu.js
@@ -12,11 +12,13 @@ class MenuConstructor {
       this.content.classList.toggle('hidden');
       this.closeButton.classList.toggle('hidden');
       this.openButton.classList.toggle('hidden');
-      console.log(this.closeButton.classList)
-      console.log('activating menu')
     }
   }
 let mainMenu = document.querySelectorAll('.menu');
 mainMenu = Array.from(mainMenu).map( mainMenuArray => new MenuConstructor(mainMenuArray));
+document.querySelectorAll('.menu-open', '.menu-close').addEventListener('click',
+function() {
+  document.querySelector('header').classList.toggle('hidden-background')
+})
 console.log('Project by:')
 console.log('  __  __  ____    ____  \n |  |/ / |    \\\  |    | \n |     \\\ |     \\\ |    | \n |__|\\\__\\\|__|\\\__\\\|____| \n')
