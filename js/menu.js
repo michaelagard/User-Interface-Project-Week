bc80deb3b65910785cac179b4056cd095f5684b4
Michael Agard <mdagard@gmail.com>
2018-08-23 21:48:37 -0400

    emergency push,

--- js/menu.js
+++ js/menu.js
@@ -1,25 +1,24 @@
 class MenuConstructor {
-    constructor(element) {
-      this.element = element;
-      this.openButton = element.querySelector('.menu-open');
-      this.closeButton = element.querySelector('.menu-close');
-      this.content = element.querySelector('.menu-content');
-      this.openButton.addEventListener('click', () => {this.toggleContent()});
-      this.closeButton.addEventListener('click', () => {this.toggleContent()});
-      this.link = element.querySelectorAll('.menu-link')
-    }
-    toggleContent() {
-      this.content.classList.toggle('hidden');
-      this.closeButton.classList.toggle('hidden');
-      this.openButton.classList.toggle('hidden');
-    }
+  constructor(element) {
+    this.element = element;
+    this.openButton = element.querySelector('.menu-open');
+    this.closeButton = element.querySelector('.menu-close');
+    this.content = element.querySelector('.menu-content');
+    this.openButton.addEventListener('click', () => {this.toggleContent()});
+    this.closeButton.addEventListener('click', () => {this.toggleContent()});
+    this.link = element.querySelectorAll('.menu-link')
   }
+  toggleContent() {
+    this.content.classList.toggle('hidden');
+    this.closeButton.classList.toggle('hidden');
+    this.openButton.classList.toggle('hidden');
+  }
+}
 let mainMenu = document.querySelectorAll('.menu');
 mainMenu = Array.from(mainMenu).map( mainMenuArray => new MenuConstructor(mainMenuArray));
 
-document.querySelectorAll('.menu-open', '.menu-close').addEventListener('click',
-function() {
-  document.querySelector('header').classList.toggle('hidden-background')
-})
-console.log('Project by:')
-console.log('  __  __  ____    ____  \n |  |/ / |    \\\  |    | \n |     \\\ |     \\\ |    | \n |__|\\\__\\\|__|\\\__\\\|____| \n')
+// document.querySelectorAll('.menu-open', '.menu-close').addEventListener('click',
+// function() {
+//   document.querySelector('header').classList.toggle('hidden-background')
+// })
+console.log('Project by:\n  __  __  ____    ____  \n |  |/ / |    \\\  |    | \n |     \\\ |     \\\ |    | \n |__|\\\__\\\|__|\\\__\\\|____| \n')
